7563f77bba3997a6edcb6a3dbd31b07e929fe8a4
Luke McMahon <luke.mcmahon@twobulls.com>
2022-08-23 15:55:09 +1000

    compelted part a

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 
+const app = express();
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
+  )
+);
 
 let people = [
   {
@@ -32,8 +40,6 @@ const generateId = () => {
 };
 
 const checkIfExists = (body) => {
-  console.log(body.name);
-
   const nameExists = people.filter((p) => p.name === body.name);
   return nameExists;
 };
